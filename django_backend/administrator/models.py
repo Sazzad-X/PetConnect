68c217e163994fba7651f413f369b9ebb436b97b
Sazzad-X <emailme.sazzad@gmail.com>
2024-11-29 18:39:45 +0600

    django views

--- django_backend/administrator/models.py
+++ django_backend/administrator/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class User(AbstractUser):
+    # Boolean fields to select the type of account.
+    is_user = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
---
+++ django_backend/administrator/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+
+urlpatterns = [
+    # path(
+    #     "registration/",
+    #     AgentRegistrationView.as_view(),
+    #     name="registration_agent",
+    # ),
+]
