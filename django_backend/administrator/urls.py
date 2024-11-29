b8117a96da015be76260f33f85ece8a087357ac7
Sazzad-X <emailme.sazzad@gmail.com>
2024-11-29 20:48:29 +0600

    admin

--- django_backend/administrator/urls.py
+++ django_backend/administrator/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-
+from .views import PetApprovalView, EncyclopediaApprovalView
 
 urlpatterns = [
-    # path(
-    #     "registration/",
-    #     AgentRegistrationView.as_view(),
-    #     name="registration_agent",
-    # ),
+    path("pet-approval/", PetApprovalView.as_view(), name="pet-approval"),
+    path("pet-approval/<int:pk>/", PetApprovalView.as_view(), name="pet-approval"),
+    path(
+        "encyclopedia-approval/",
+        EncyclopediaApprovalView.as_view(),
+        name="encyclopedia-approval",
+    ),
+    path(
+        "encyclopedia-approval/<int:pk>/",
+        EncyclopediaApprovalView.as_view(),
+        name="encyclopedia-approval",
+    ),
 ]
--- django_backend/administrator/views.py
+++ django_backend/administrator/views.py
@@ -7,7 +7,8 @@ from rest_framework.permissions import BasePermission, IsAuthenticated
 from dj_rest_auth.views import PasswordChangeView
 from rest_framework import status
 
-# from .serializers import CustomPasswordChangeSerializer
+from adoption.models import Pet, Encyclopedia
+from adoption.serializers import PetSerializer, EncyclpediaSerializer
 
 
 # Authenticate User Only Class
@@ -20,3 +21,45 @@ class AuthenticateOnlyAdmin(BasePermission):
                 return False
 
         return False
+
+
+class EncyclopediaApprovalView(APIView):
+    permission_classes = [AuthenticateOnlyAdmin]
+
+    def get(self, request):
+        encyclopedias = Encyclopedia.objects.filter(is_approved=False)
+        serializer = EncyclpediaSerializer(encyclopedias, many=True)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        encyclopedia = Encyclopedia.objects.get(pk=pk)
+        encyclopedia.is_approved = True
+        encyclopedia.save()
+        return Response(status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        encyclopedia = Encyclopedia.objects.get(pk=pk)
+        encyclopedia.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class PetApprovalView(APIView):
+    permission_classes = [AuthenticateOnlyAdmin]
+
+    def get(self, request):
+        pets = Pet.objects.filter(is_approved=False)
+        serializer = PetSerializer(pets, many=True)
+        return Response(serializer.data)
+
+    # approve pet on put request
+    def put(self, request, pk):
+        pet = Pet.objects.get(pk=pk)
+        pet.is_approved = True
+        pet.status = "Approved"
+        pet.save()
+        return Response(status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        pet = Pet.objects.get(pk=pk)
+        pet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
