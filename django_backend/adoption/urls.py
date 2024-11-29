3b0194fb46fc2bf1f86591adeb2e6d944984cebe
Sazzad-X <emailme.sazzad@gmail.com>
2024-11-29 18:54:02 +0600

    views modified

--- django_backend/adoption/urls.py
+++ django_backend/adoption/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from adoption.views import UserRegistrationSerializer
+from adoption.views import UserRegistrationView
 
 
 urlpatterns = [
     path(
         "registration/",
-        UserRegistrationSerializer.as_view(),
+        UserRegistrationView.as_view(),
         name="registration_user",
     ),
 ]
